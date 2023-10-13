98127992a0056d8506950c522cb6dac915894ca4
Konrad <117399047+PandakiraDev@users.noreply.github.com>
2023-10-14 00:14:58 +0200

    module4fixes

--- src/app/products/[pageNumber]/loading.tsx
+++ src/app/products/[pageNumber]/loading.tsx
@@ -1,6 +1,9 @@
 export default function Loading() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div
+      className="w-screen h-screen flex items-center justify-center"
+      aria-busy="true"
+    >
       {" "}
       <div className="animate-spin w-12 h-12 bg-red-100">Loading...</div>
     </div>
